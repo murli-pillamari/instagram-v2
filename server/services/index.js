91f5954f87e753ea1f4b02b24992c7e658fc4870
murlipillamari <murlipillamari6@gmail.com>
2023-11-27 20:07:49 +0530

    added server to listen API

---
+++ server/services/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+const users = [
+    {
+        id:1,
+        name: 'Murli',
+    },
+    {
+        id:2,
+        name: 'Murli 2',
+    },
+    {
+        id:3,
+        name: 'Murli 3',
+    },
+];
+
+app.get('/',(req,res) => {
+    res.send('Welcome to our instagram API !')
+})
+
+app.get('api/users',(req,res) => {
+    res.json(users)
+})
+
+app.get('api/users:id',(req,res) => {
+    const { id } = req.params || {};
+    const user = users.find((item) => item.id === id );
+
+    res.json(user);
+})
+
+app.listen('3000',() => {  
+    console.log("Starting the sever!")
+})
