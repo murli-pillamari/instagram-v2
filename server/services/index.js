15a7cadcebcbb3b3e01016d401a71d2004009ca4
murlipillamari6 <murlipillamari6@gmail.com>
2023-11-29 00:02:38 +0530

    API changes

--- server/services/index.js
+++ server/services/index.js
@@ -1,36 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const users = [
-    {
-        id:1,
-        name: 'Murli',
-    },
-    {
-        id:2,
-        name: 'Murli 2',
-    },
-    {
-        id:3,
-        name: 'Murli 3',
-    },
+  {
+    id: 1,
+    name: "Murli",
+  },
+  {
+    id: 2,
+    name: "Murli 2",
+  },
+  {
+    id: 3,
+    name: "Murli 3",
+  },
 ];
 
-app.get('/',(req,res) => {
-    res.send('Welcome to our instagram API !')
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to our instagram API !");
+});
 
-app.get('api/users',(req,res) => {
-    res.json(users)
-})
+app.get("/api/users", (req, res) => {
+  res.json(users);
+});
 
-app.get('api/users:id',(req,res) => {
-    const { id } = req.params || {};
-    const user = users.find((item) => item.id === id );
+app.get("/api/users/:id", (req, res) => {
+  const { id } = req.params || {};
+  const user = users.find((item) => item.id == id);
 
-    res.json(user);
-})
+  res.json(user);
+});
 
-app.listen('3000',() => {  
-    console.log("Starting the sever!")
-})
+app.listen("3000", () => {
+  console.log("Starting the sever!");
+});
